a63cded0c8ba4a727d46be75ffc601af0f4ed846
Jeremie Miller <jeremie@jabber.org>
2013-03-08 06:05:29 -0600

    add who/key support, wrap ursa for crypto, sends and receives packets now

--- telehash.js
+++ telehash.js
@@ -1,31 +1,52 @@
-var dgram = require('dgram');
-var os = require('os');
-var dhash = require('./dhash').Hash;
+var dgram = require("dgram");
+var os = require("os");
+var async = require("async");
+var ursa = require("ursa")
+var dhash = require("./dhash").Hash;
+
+var REQUEST_TIMEOUT = 5 * 1000; // default timeout for any request
+var warn = console.log; // switch to function(){} to disable
+var debug = function(){}; // switch to console.log to enable
 
 exports.hash = function(string)
 {
   return new dhash(string);
 }
 
-// start a hashname listening and ready to go
-exports.hashname = function(space, keys, args)
+// simple handy wrapper utility to make a new keypair
+exports.createKeys = function(mod, exp)
 {
-  if(!space || !keys || !keys.public || !keys.private) return undefined;
+  var key = ursa.generatePrivateKey(mod, exp);
+  return {public:key.toPublicPem("utf8"), private:key.toPrivatePem("utf8")};
+}
+
+// start a hashname listening and ready to go
+exports.hashname = function(space, privateKey, args)
+{
+  if(!space || !privateKey) return undefined;
   if(!args) args = {};
 
   // configure defaults
-  var self = {space:space, keys:keys, cb:{}};
-  self.hashname = new dhash(keys.public+space).toString();
+  var self = {space:space, cb:{}, operators:[], watch:{}};
+  // parse/validate the private key
+  try {
+    self.ukey = ursa.coercePrivateKey(privateKey);
+    self.pubkey = self.ukey.toPublicPem("utf8");
+  } catch(E) {
+    warn("couldn't parse key:", E);
+    return undefined;
+  };
+  self.hashname = new dhash(self.pubkey+space).toString();
   if (!args.ip || args.natted) self.nat = true;
   self.ip = args.ip || "0.0.0.0";
   self.port = parseInt(args.port) || 0;
 
+
   // udp socket
   self.server = dgram.createSocket("udp4", function(msg, rinfo){
-    var from = rinfo.address + ":" + rinfo.port;
     var packet = decode(msg);
-    if(!packet) return console.log("failed to decode a packet from", from);
-    packet.from = from;
+    if(!packet) return warn("failed to decode a packet from", rinfo.address, rinfo.port, msg.toString());
+    packet.from = {ip:rinfo.address, port:rinfo.port};
     incoming(self, packet);
   });
   self.server.bind(self.port, self.ip);
@@ -45,9 +66,70 @@ exports.hashname = function(space, keys, args)
   self.address = [self.hashname, self.ip, self.port].join(",");
 
   // set up methods (personal prefernce to do this explicitly vs. prototype pattern)
-  self.lookup = function(callback) { this.cb.lookup = callback }
+  self.myLookup = function(callback) { this.cb.lookup = callback };
+  self.setOperators = function(operators) { this.operators = operators.map(parseAddress) || [] };
+  self.doWho = function(hn, callback) { who(this, hn, callback) };
 
   return self;
+}
+
+// perform a who request
+function who(self, hn, callback)
+{
+  var key;
+  async.forEachSeries(self.operators, function(op, cbOps){
+    var body = new Buffer(JSON.stringify({hashname:hn, from:self.hashname, to:op.hashname, space:self.space, x:Date.now()+10000}));
+    var js = {sig:self.ukey.hashAndSign("md5", body).toString("base64")};
+    js.who = new dhash(js.sig).toString();
+    keywatch(self, js.who, function(err, value){
+      if(value) key = value;
+      cbOps(value); // stops async when we get a value
+    });
+    send(self, op, encode(js, body));
+  }, function(){
+    if(!key) return callback("not found");
+    callback(null, key);
+  });
+}
+
+function keywatch(self, key, callback)
+{
+  var timeout = setTimeout(function(){done("timeout")}, REQUEST_TIMEOUT);
+  function done(err, value)
+  {
+    if(!timeout) return; // re-entered by accident if answer came after timeout
+    clearTimeout(timeout);
+    timeout = false;
+    delete self.watch[key];
+    callback(err, value);
+  }
+  
+  self.watch[key] = {done:done, parts:[]};
+}
+
+// wiring wrapper
+function send(self, to, buf)
+{
+  self.server.send(buf, 0, buf.length, to.port, to.ip);
+}
+
+// just parse the "60518c1c11dc0452be71a7118a43ab68e3451b82,172.16.42.34,65148" format
+function parseAddress(str)
+{
+  var parts = str.split(",");
+  return {hashname:parts[0], ip:parts[1], port:parseInt(parts[2])};
+}
+
+// create a wire writeable packet
+function encode(js, body)
+{
+  debug("ENCODING", js, body && body.length);
+  var jsbuf = new Buffer(JSON.stringify(js), "utf8");
+  if(typeof body === "string") body = new Buffer(body, "utf8");
+  body = body || new Buffer(0);
+  var len = new Buffer(2);
+  len.writeInt16BE(jsbuf.length, 0);
+  return Buffer.concat([len, jsbuf, body]);
 }
 
 // decode a packet from a buffer
@@ -60,11 +142,11 @@ function decode(buf)
   // parse out the json
   var packet = {};
   try {
-      packet.json = JSON.parse(buf.toString("utf8",2,len+2));
+      packet.js = JSON.parse(buf.toString("utf8",2,len+2));
   } catch(E) {
     return undefined;
   }
-  
+
   // if any body, attach it as a buffer
   if(buf.length > (len + 2)) packet.body = buf.slice(len + 2);
   
@@ -74,5 +156,45 @@ function decode(buf)
 // figure out what this packet is and have some fun
 function incoming(self, packet)
 {
-  console.log(self.address, "packet from", packet.from, packet.json);
+  debug("INCOMING", self.hashname, "packet from", packet.from, packet.js, packet.body && packet.body.length);
+  
+  if(packet.js.who) inWho(self, packet);
+  if(packet.js.key) inKey(self, packet);
+  
+}
+
+function inKey(self, packet)
+{
+  var watch = self.watch[packet.js.key];
+  if(!watch) return warn("unknown key from", packet.from);
+
+  // some sanity checks
+  if(!packet.body) return warn("missing key body from", packet.from);
+  var seq = parseInt(packet.js.seq || 0);
+  if(seq === NaN || seq < 0 || seq > 10) return warn("invalid seq", packet.js.seq, packet.from);
+
+  watch.parts[seq] = packet.body.toString("utf8");
+
+  // check if it's a valid public key yet
+  var key = watch.parts.join("");
+  try { ursa.coercePublicKey(key) } catch(E) { return warn(E)};
+  watch.done(null, key);
+}
+
+function inWho(self, packet)
+{
+  try { packet.body = JSON.parse(packet.body.toString()) } catch(E) { return warn("invalid body from", packet.from); }
+  
+  function valued(err, key)
+  {
+    // split into 1k chunks max
+    var chunks = [].concat.apply([], key.split('').map(function(x,i){ return i%1000 ? [] : key.slice(i,i+1000) }));
+    for(var i = 0; i < chunks.length; i++)
+    {
+      send(self, packet.from, encode({key:packet.js.who, seq:i}, chunks[i]));
+    }
+  }
+  
+  if(packet.body.hashname === self.hashname) return valued(null, self.pubkey);
+  if(self.cb.lookup) return self.cb.lookup(packet.body.hashname, valued);
 }
--- test.js
+++ test.js
@@ -2,9 +2,14 @@
 var th = require("./telehash");
 var keypair = require(process.argv[2]);
 
-var test = th.listen("test", keypair);
+var test = th.hashname("test", keypair);
 console.log(test.address);
+test.setOperators(["1e9f22cdb675bd9d67ec10c2f21c020acc0bd20f,172.16.42.34,58625"]);
+test.doWho("1e9f22cdb675bd9d67ec10c2f21c020acc0bd20f", function(err, key){
+  console.log("WHOM", err, key);
+});
 
+/*
 var hash = require("./hash");
 var a = new hash.Hash(null, "a9993e364706816aba3e25717850c26c9cd0d89d");
 var b = new hash.Hash(null, a.far());
@@ -24,3 +29,4 @@ var x = [x3, x1, x2];
 x.forEach(function(h){console.log(h.toString())});
 var y = x.sort(function(s1, s2){ return a.distanceTo(s1) - a.distanceTo(s2) });
 y.forEach(function(h){console.log(h.toString())});
+*/

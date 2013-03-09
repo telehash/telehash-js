e35403b0b83df6480a0a68be82148149193471f8
Jeremie Miller <jeremie@jabber.org>
2013-03-09 00:33:19 -0600

    just more test scripts

---
+++ demo/anya.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+var tele = require("../telehash");
+
+// what is our operator, pass in or replace this value
+var opaddress = process.argv[2];
+
+// load up our private key
+var ckeys = require("./anya.json");
+
+// start up our client hashname in the same space
+var client = tele.hashname("testing.private", ckeys.private);
+
+// provide the operator(s) for this hashname
+client.setOperators([opaddress]);
+
+// ask for ourselves, which will query the operator
+client.doWho(client.hashname, function(err, pubkey){
+	if(err) return console.log("failed to find our hashname in this space:", err);
+	if(pubkey !== ckeys.public) return console.log("odd, our keys didn't match", pubkey, ckeys.public); 
+	console.log("great, we're connected! our address is", client.address);
+	client.doLine(opaddress.split(",")[0], function(err){
+  	if(err) return console.log("failed to open line:", err);
+	  console.log("line opened");
+	})
+});
---
+++ demo/ollie.js
@@ -0,0 +1,14 @@
+var tele = require("../telehash");
+var opkeys = require("./ollie.json");
+var operator = tele.hashname("testing.private", opkeys.private, {port:42424});
+console.log("ollie address is ", operator.address);
+
+Object.keys(opkeys).map(function(key){
+  var hn = tele.hash(opkeys[key]+"testing.private").toString();
+  opkeys[hn] = opkeys[key];
+});
+operator.myLookup(function(hn, callback){
+  console.log("lookup",hn)
+	if (opkeys[hn]) return callback(null, opkeys[hn]);
+	callback("not found");
+});

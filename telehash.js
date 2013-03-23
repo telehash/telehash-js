85aacec8186ee4ef04c52a6fd2e922bb615fb1d9
Jeremie Miller <jeremie@jabber.org>
2013-03-23 00:30:00 -0500

    write buffering for streams works, and http proxy now supports streaming

--- telehash.js
+++ telehash.js
@@ -91,6 +91,7 @@ exports.hashname = function(space, privateKey, args)
     self.proxyBase = args;
   };
   self.doProxy = function(args, callback) { return doProxy(self, args, callback) };
+  self.doProxyStream = function(args, callback) { return doProxyStream(self, args, callback) };
 
 
   return self;
@@ -126,6 +127,32 @@ function doProxy(self, args, callback)
   });
 }
 
+// ask hashname to answer an http request, streamingly
+function doProxyStream(self, args, callback)
+{
+  if(!args || !args.path ||!args.hashname) return callback("no path or hashname");
+  // create stream and a req json
+  var stream = getStream(self, seen(self, args.hashname));
+  var js = {req:{}};
+  js.req.p = args.path;
+  js.req.m = args.method || "GET";
+  if(args.headers) js.req.h = args.headers;
+
+  // give back node streaming interface
+  var x = wrapStream(self, stream, function(packet){
+    // when the headers show up pass them along
+    if(packet.js.res)
+    {
+      var res = {};
+      res.statusCode = packet.js.res.s || 500;
+      res.headers = packet.js.res.h || {};
+      callback(res);
+    }
+  });
+  x.js(js); // send headers
+  return x;
+}
+
 // ask hashname to open a socket to this ip:port
 function doSocket(self, args, callback)
 {
@@ -154,25 +181,82 @@ function doStream(self, hashname, callback)
 }
 
 // use node's stream api wrapper
-function wrapStream(self, stream)
+function wrapStream(self, stream, cbExtra)
 {
   var duplex = new require("stream").Duplex();
+
+  // allow for manually injected json
+  duplex.bufJS = {};
+  duplex.js = function(js){
+    Object.keys(js).forEach(function(key){ duplex.bufJS[key] = js[key]; });
+    setTimeout(doChunk, 10);
+  };
+  
+  // buffer writes and chunk them out
+  duplex.bufBody = new Buffer(0);
+  duplex.cbWrite;
+
+  function doChunk(){
+    console.log("CHUNKING", duplex.bufJS, duplex.bufBody.length)
+    if(duplex.bufBody.length === 0 && Object.keys(duplex.bufJS).length === 0) return;      
+    var bodyout;
+    var jsout = duplex.bufJS;
+    duplex.bufJS = {};
+    if(duplex.bufBody.length > 0)
+    {
+      var len = 1024 - JSON.stringify(jsout).length; // max body size for a packet
+      if(duplex.bufBody.length < len) len = duplex.bufBody.length;
+      bodyout = duplex.bufBody.slice(0, len);
+      duplex.bufBody = duplex.bufBody.slice(len, duplex.bufBody.length - len);
+    }
+    // send it!
+    sendStream(self, stream, {js:jsout, body:bodyout, done:function(){
+      // we might be backed up, let more in
+      if(duplex.cbWrite)
+      {
+        // am I being paranoid that a cbWrite() could have set another duplex.cbWrite?
+        var cb = duplex.cbWrite;
+        delete duplex.cbWrite;
+        cb();
+      }
+    }});
+    // recurse nicely
+    setTimeout(doChunk, 10);
+    
+  };
+
+  duplex.end = function(){
+    duplex.bufJS.end = true;
+    if(stream.errMsg) duplex.bufJS.err = stream.errMsg;
+    doChunk();
+  }
+
+  duplex._write = function(buf, enc, cbWrite){
+    duplex.bufBody = Buffer.concat([duplex.bufBody, buf]);
+
+    // if there's 50 packets waiting to be confirmed, hold up here, otherwise buffer up
+    var cbPacket = doChunk;
+    if(stream.outq.length > 50)
+    {
+      duplex.cbWrite = cbWrite;
+    }else{
+      cbWrite();
+    }
+    
+    // try sending a chunk;
+    doChunk();
+  }  
+  
   duplex._read = function(size){
     // TODO handle backpressure
     // perform duplex.push(body)'s if any waiting, if not .push('')
     // handle return value logic properly
   };
-  duplex._write = function(buf, enc, cb){
-    console.log("WRTEME",buf.toString());
-    // TODO backpressure buffering, call cb() for our own buffer until it's full
-    sendStream(self, stream, {js:{}, body:buf, done:function(){console.log("WROTE");cb()}});
-  }
-  duplex.end = function(){
-    sendStream(self, stream, {js:{end:true, err:stream.errMsg}});
-  }
+
   stream.handler = function(self, packet, cbHandler) {
     // TODO migrate to _read backpressure stuff above
     console.log("HANDLER", packet.js)
+    if(cbExtra) cbExtra(packet);
     if(packet.body) duplex.push(packet.body);
     if(packet.js.end) duplex.push(null);
     cbHandler();
@@ -193,7 +277,8 @@ function getStream(self, to, id)
   stream.q = async.queue(function(packet, cbQ){
     inStreamSeries(self, stream, packet, cbQ);
   }, 1);
-  // handy util
+
+  // handy utils, send just one anytime explicitly
   stream.send = function(js, body){ sendStream(self, stream, {js:js, body:body}) };
 
   to.streams[stream.id] = stream;
@@ -202,8 +287,6 @@ function getStream(self, to, id)
 
 function sendStream(self, stream, packet)
 {
-  if(stream.outq > 100) return warn("dropping outgoing packet since the stream buffer is too large", stream.id, packet.to);
-
   packet.js.stream = stream.id;
   packet.js.seq = stream.outSeq++;
   packet.js.ack = stream.inSeq;
@@ -657,7 +740,7 @@ function inProxy(self, packet, callback)
   function err(msg)
   {
     debug("PROXY ERROR", msg);
-    packet.stream.send({end:true, res:{"s":500}}, msg);
+    packet.stream.send({end:true, res:{"s":500}}, msg.toString());
   }
 
   // new request

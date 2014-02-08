21faa0ed7937e6cfeb37c45a85cfc6e31fec00f9
Jeremie Miller <jeremie@jabber.org>
2014-02-08 15:31:05 -0700

    optional set selection for creation

--- seed/seed.js
+++ seed/seed.js
@@ -10,11 +10,18 @@ var argv = require("optimist")
   .boolean("bridge").default("bridge",true)
   .boolean("v").describe("v", "verbose")
   .boolean("nolan").describe("nolan", "disable lan usage")
+  .string("cs").describe("cs", "cipher sets to generate id with, comma delim")
   .describe("ip", "force set the public IP address to override any NAT detection")
   .argv;
 
 if(argv.v) tele.debug(console.log);
 tele.info(function(){console.log.apply(console,arguments)});
+var sets;
+if(typeof argv.cs == "string")
+{
+  sets = {};
+  argv.cs.split(",").forEach(function(cs){sets[cs]=true});
+}
 
 if(argv.port == 42420)
 {
@@ -34,7 +41,7 @@ if(fs.existsSync(idfile) && (keys = require(idfile)) && keys.parts)
   tele.genkeys(function(err, keys){
     fs.writeFileSync(idfile, JSON.stringify(keys, null, 4));
     init(keys);
-  });
+  },false,sets);
 }
 
 function init(keys)
@@ -44,7 +51,7 @@ function init(keys)
   if(argv.seeds) seed.addSeeds(argv.seeds);
   if(!argv.nohttp) seed.http(argv.http, require('socket.io').listen(argv.port, {log:false}));
   seed.bridging = argv.bridge;
-  seed.online(function(err){
+  seed.online(function(err,count){
     var lan4 = seed.paths.lan4 || {};
     var pub4 = seed.paths.pub4 || {};
     var ip = pub4.ip||lan4.ip;
@@ -54,8 +61,8 @@ function init(keys)
     if(seed.paths.lan6) info.paths.push({type:"ipv6",ip:seed.paths.lan6.ip,port:seed.paths.lan6.port});
     if(!argv.nohttp) info.paths.push({type:"http",http:seed.paths.http.http});
     if(seed.bridging) info.bridge = true;
-    console.log(JSON.stringify(info, null, 4));
+    console.log("[\n\t"+JSON.stringify(info)+"\n]");
     if(seed.nat) console.log("warning, may be behind a NAT, IP and Port may not be stable");
-    console.log((err?err:"connected to mesh seed peers"));
+    console.log((err?err:"connected to "+count+" mesh seed peers"));
   });
 }

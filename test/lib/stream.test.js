90be9766be06ad5e2cef07067a66b4416805238c
Jeremie Miller <jeremie@jabber.org>
2014-09-25 08:51:23 -0600

    fixes to stream and an actual (broken) test

--- test/lib/stream.test.js
+++ test/lib/stream.test.js
@@ -1,7 +1,13 @@
 var expect = require('chai').expect;
+var concat = require('concat-stream');
+var fs = require('fs');
+var telehash = require('../../telehash.js');
 var stream = require('../../lib/stream.js');
 
 describe('telehash/stream', function(){
+
+  var idA = {"keys":{"1a":"akndnx5kbansip6xphxymwckpqkjj26fcm"},"secrets":{"1a":"ksxslm5mmtymnbph7nvxergb7oy3r35u"},"hashname":"5uegloufcyvnf34jausszmsabbfbcrg6fyxpcqzhddqxeefuapvq"};
+  var idB = {"keys":{"1a":"apkoh54rkobkeela6d62hblhqd7grqd5dm"},"secrets":{"1a":"ljfm3ov42x2fl6gsg6bxiqgtstnxls7r"},"hashname":"fvifxlr3bsaan2jajo5qqn4au5ldy2ypiweazmuwjtgtg43tirkq"};
 
   it('should export an extension', function(){
     expect(stream).to.be.a('object');
@@ -17,6 +23,31 @@ describe('telehash/stream', function(){
       expect(ext.open.stream).to.be.a('function');
       expect(ext.link).to.be.a('function');
     });
+  });
+
+  it('should stream itself between two', function(done){
+    telehash.log({debug:console.log});
+    var meshA = telehash.mesh({id:idA,extensions:{stream:stream}});
+    expect(meshA).to.exist;
+    var meshB = telehash.mesh({id:idB,extensions:{stream:stream}});
+    expect(meshB).to.exist;
+    
+    // pair them
+    meshA.mesh(meshB);
+    var linkAB = meshA.link(meshB.hashname);
+    
+    // accept and concat stream
+    meshB.stream(function(link, req, accept){
+      expect(link).to.exist;
+      accept().pipe(concat(function(me){
+        console.log('MEMEME',me);
+        done();
+      }));
+    })
+    
+    // send stream
+    fs.createReadStream(__filename).pipe(linkAB.stream());
+
   });
 
 });

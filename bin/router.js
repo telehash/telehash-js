4c538357926e79855c937d65e781cae7a283c072
Jeremie Miller <jeremie@jabber.org>
2014-09-27 15:51:10 -0600

    move command/repl code into the lib so it can be shared

--- bin/router.js
+++ bin/router.js
@@ -1,67 +1,11 @@
 #!/usr/bin/env node
 
-exports = {}; // testing
+var repl = require('../lib/repl.js');
 
-var telehash = require('..');
-var fs = require('fs');
-var path = require('path-extra');
-var repl = require('repl');
-
-// do any generic router specialization on a basic mesh
-exports.router = function(args, cbRouter)
-{
-  telehash.load(args, function(err, mesh){
-    mesh.json().router = true;
-    mesh.discover(true);
-    cbRouter(err, mesh);
-  });
-}
-
-// if loading as a module, don't do script stuff
-if(module.parent) return;
-
-// we're a script, take args and start
 var argv = require('optimist')
   .default('port', 42424)
   .boolean('norepl').describe('norepl', 'disable REPL')
   .boolean('v').describe('v', 'verbose')
   .argv;
 
-if(argv.port == 42420)
-{
-  console.log('that port is reserved');
-  process.exit(1);
-}
-
-// localize our id file
-argv.id = (argv.id) ? path.resolve(argv.id) : path.join(path.homedir(),'.router.json');
-argv.links = (argv.links) ? path.resolve(argv.links) : path.join(path.homedir(),'.links.json');
-
-if(argv.v)
-{
-  telehash.log({debug:console.log});
-  console.log('router starting with args',argv);
-}
-
-exports.router(argv, function(err, mesh){
-  if(err)
-  {
-    console.log('something went wrong :(',err,argv);
-    return process.exit(1);
-  }
-  mesh.log.info(mesh.json({space:2}));
-  mesh.log.info('router up');
-  if(argv.norepl !== false) return;
-  var r = repl.start({
-    prompt: mesh.hashname.substr(0,8)+'> ',
-    input: process.stdin,
-    output: process.stdout
-  });
-  r.context.mesh = mesh;
-  r.on('exit', function () {
-    // TODO any nice shutdown?
-    console.log(); // nice newline
-    process.exit(0);
-  });
-});
-
+repl.start(argv);

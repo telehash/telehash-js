6297fe2cb6aacafc6fcdc24b947b0d5a2aeb6652
Jeremie Miller <jeremie@jabber.org>
2014-09-28 08:21:52 -0600

    add cleaner repl console output

--- bin/router.js
+++ bin/router.js
@@ -12,8 +12,5 @@ repl.start(argv, function(mesh){
   mesh.json().router = true;
   mesh.discover(true);
 
-  mesh.log.info();
-  mesh.log.info(mesh.json({space:2}));
-  mesh.log.info('router up');
-  mesh.r.displayPrompt();
+  mesh.rlog('router:',mesh.json());
 });

86aa20a974b9094f547819dd41dffb7619221faf
Jeremie Miller <jeremie@jabber.org>
2014-09-30 21:06:16 -0600

    implemented most of thtp proxying, nifty

--- ext/thtp.js
+++ ext/thtp.js
@@ -1,41 +1,111 @@
 var urllib = require('url');
 var httplib = require('http');
+var streamlib = require('stream');
+var lob = require('lob-enc');
+var hashname = require('hashname');
 
 exports.name = 'thtp';
 
 exports.mesh = function(mesh, cbMesh)
 {
-  var ext = {};
-
-  // TODO use mesh.streamize
+  var ext = {open:{}};
 
   ext.link = function(link, cbLink)
   {
 
-    // proxy an existing http://nodejs.org/api/http.html#http_http_incomingmessage request/response pair
+    // proxy an existing node http request and send to a result
     link.proxy = function(req, res)
     {
-      // if not res, create a res and fire 'response' event
+      // create the thtp request json
+      var json = {};
+      if(typeof req.headers == 'object') Object.keys(req.headers).forEach(function(header){
+        json[header.toLowerCase()] = req.headers[header];
+      });
+      json[':method'] = (req.method || 'GET').toUpperCase();
+      // convenience pattern
+      if(req.url)
+      {
+        var url = urllib.parse(req.url);
+        json[':path'] = url.path;
+      }else{
+        json[':path'] = (req.path || '/');
+      }
+      var packet = lob.encode(json, false);
+      
+      // create the channel request
+      var open = {json:{type:'thtp'}};
+      open.json.seq = 0; // always reliable
+      open.body = packet.slice(0,1000); // send as much of the headers as we can
+      var channel = link.x.channel(open);
+
+      // create a stream to encode the http->thtp
+      var sencode = mesh.streamize(channel);
+      // create a stream to decode the thtp->http
+      var sdecode = lob.stream(function(packet, cbStream){
+        // mimic http://nodejs.org/api/http.html#http_http_incomingmessage
+        stream.statusCode = parseInt(packet.json[':status'])||500;
+        stream.reasonPhrase = packet.json[':reason']||'';
+        delete packet.json[':status'];
+        delete packet.json[':reason'];
+        stream.headers = packet.json;
+
+        // direct response two ways depending on args
+        if(typeof res == 'object')
+        {
+          res.writeHead(statusCode, packet.json);
+          stream.pipe(res);
+        }else if(typeof res == 'function'){
+          res(stream); // handler must set up stream piping
+        }else{
+          return cbStream('no result handler');
+        }
+        cbStream();
+      });
+
+      // any response is decoded
+      sencode.pipe(sdecode);
+
+      // finish sending the open
+      channel.send(open);
+      // if more header data, send it too
+      if(packet.length > 1000) sencode.write(packet.slice(1000));
+
+      // auto-pipe in any request body
+      if(typeof req.pipe == 'function') req.pipe(sencode);
+      
+      return sencode;
     }
     
     // create a new request just like http://nodejs.org/api/http.html#http_http_request_options_callback
     link.request = function(options, cbRequest)
     {
-      // is options a string, parse as url
-      // create a request
-      // if cbRequest, set .on('response',cbRequest);
-      // call link.proxy()
-      // return req
+      // allow string url as the only arg
+      if(typeof options == 'string') options = urllib.parse(options);
+      if(!options.method) options.method = 'GET';
+      // TODO, handle friendly body/json options like the request module?
+      var proxy = link.proxy(options, function(response){
+        if(cbRequest) cbRequest(undefined, response);
+        cbRequest = false;
+      });
+      proxy.on('error', function(err){
+        if(cbRequest) cbRequest(err);
+        cbRequest = false;
+      });
+
+      // friendly
+      if(options.method.toUpperCase() == 'GET') proxy.end();
+      return proxy;
     }
 
     cbLink();
   }
 
   // accept a request to a url where the hashname is the hostname, calls link.request
-  mesh.request = function(req, res)
+  mesh.request = function(req, cbRequest)
   {
-    // is req a string, or req.hostname is hashname, or req.url
-    // get the link, then link.request(options, cbRequest)
+    if(typeof req == 'string') req = urllib.parse(req);
+    if(!hashname.isHashname(req.hostname)) return cbRequest('invalid hashname',req.hostname);
+    return mesh.link(req.hostname).request(req, cbRequest);
   }
   
   // start accepting incoming thtp requests
@@ -61,214 +131,63 @@ exports.mesh = function(mesh, cbMesh)
     // handler for incoming thtp channels
     ext.open.thtp = function(args, open, cbOpen){
       var link = this;
-      // TODO, create channel and receive until we have a request
-      // add header id'ing requesting hashname
-      // create a res stream
-      // proxy.emit('request',req, res);
+      var channel = link.x.channel(open);
+      // pipe the channel into a decoder, then handle it
+      var req = mesh.streamize(channel).pipe(lob.stream(function(packet, cbStream){
+
+        // mimic http://nodejs.org/api/http.html#http_http_incomingmessage
+        req.method = packet.json[':method'];
+        req.url = packet.json[':path'];
+        req.headers = packet.json;
+        req.headers['x-hashname'] = link.hashname; // for any http handler visibility
+
+        // now mimic http://nodejs.org/api/http.html#http_class_http_serverresponse
+        var res = new streamlib.Transform();
+        res.pipe(req); // any output goes back
+
+        // write out the header bytes first
+        res.writeHead = function(statusCode, reasonPhrase, headers)
+        {
+          // don't double!
+          if(res.statusCode) return mesh.log.warn('double call to thtp writeHead',(new Error()).stack);
+          // be friendly
+          if(!headers && typeof reasonPhrase == 'object')
+          {
+            headers = reasonPhrase;
+            reasonPhrase = false;
+          }
+          res.statusCode = parseInt(statusCode)||500;
+
+          // construct the thtp response
+          var json = {};
+          json[':status'] = res.statusCode;
+          if(reasonPhrase) json[':reason'] = reasonPhrase;
+          if(headers) Object.keys(headers).forEach(function(header){
+            json[header.toLowerCase()] = headers[header];
+          });
+          
+          // send it
+          res.push(lob.encode(json, false));
+        }
+
+        // just ensure headers are written before sending data
+        res._transform = function(data,enc,cbTransform)
+        {
+          if(!res.statusCode) res.writeHead(200);
+          res.push(data);
+        }
+
+        // show the bouncer our fake id
+        proxy.emit('request', req, res);
+        
+        cbStream();
+      }));
+
+      channel.receive(open); // actually opens it and handles any body in the stream
+      cbOpen();
     }
 
   }
 
   cbMesh(undefined, ext);
-
-  /*
-  self.thtp.request = function(args, cbRequest)
-  {
-    if(!cbRequest) cbRequest = function(){};
-    if(typeof args == "string") args = {uri:args}; // convenience
-    if(typeof args != "object" || !(args.uri || args.url || args.hashname)) return errored("invalid args",cbRequest);
-
-    if(args.hashname) args.uri = "thtp://"+args.hashname+args.path;
-    var uri = self.uriparse(args.uri||args.url);
-
-    if(uri.protocol != "thtp:") return errored("invalid protocol "+uri.protocol,cbRequest);
-    if(uri.hostname == self.hashname) return errored("can't request self",cbRequest);
-    var to;
-    if(!(to = self.whois(uri.hostname))) return errored("invalid hashname",cbRequest);
-
-    var js = {};
-    if(typeof args.headers == "object") Object.keys(args.headers).forEach(function(header){
-      js[header.toLowerCase()] = args.headers[header].toString();
-    });
-    if(args.body) js["content-length"] = args.body.length.toString();
-    js.method = args.method || "get";
-    js.path = uri.path;
-
-    var body = self.pencode(js,args.body);
-    js = {};
-
-    // single-shot requests
-    if(body.length <= 1000) js.end = true;
-
-    var pin = new Buffer(0);
-//    console.log("REQ",js,body);
-    var pipe = streamer(to.start("thtp",{bare:true,js:js,body:body.slice(0,1000)},function(err,packet,chan,cbChan){
-      cbChan(true);
-//      console.log("PACKET",packet.js,packet.body.length)
-      if(pipe.headers)
-      {
-        pipe.push(packet.body);
-        if(err) pipe.emit("end");
-        return;
-      }
-
-      if(packet.body) pin = Buffer.concat([pin,packet.body]);
-      var http;
-      if(!(http = self.pdecode(pin)))
-      {
-        // no packet (yet)
-        if(err) cbRequest(500,pipe);
-        return;
-      }
-
-      pipe.status = parseInt(http.js.status) || 500;
-      pipe.headers = http.js;
-      cbRequest(pipe.status >= 300?pipe.status.toString():false,pipe); // flag error for status too
-      if(http.body) pipe.push(http.body);
-      if(err) pipe.emit("end");
-    }));
-
-    // any remainder
-    if(body.length > 1000) pipe.end(body.slice(1000));
-
-    return pipe;
-  }
-
-  self.thtp.listen = function(cbListen)
-  {
-    self.rels["thtp"] = function(err, packet, chan, cbStart)
-    {
-
-      var pipe;
-      var pin = new Buffer(0);
-      chan.callback = function(err, packet, chan, cbChan)
-      {
-        cbChan(true);
-        // just streaming the body
-        if(pipe)
-        {
-          if(packet.body) pipe.push(packet.body);
-          if(err) pipe.emit("end");
-          return;          
-        }
-        
-        // if parsing the request yet
-        pin = Buffer.concat([pin,packet.body]);
-        if(!(http = self.pdecode(pin)))
-        {
-          if(err) chan.end();
-          return;
-        }
-
-//        console.log("REQ",http,http.js);
-        // new thtp request
-        if(typeof http.js.method != "string" || typeof http.js.path != "string") return chan.err("invalid");
-
-        pipe = streamer(chan);
-        pipe.method = http.js.method;
-        pipe.path = http.js.path;
-        delete http.js.method;
-        delete http.js.path;
-        pipe.headers = http.js;
-        cbListen(pipe,function(args){
-          if(!args) args = {};
-          if(args.err) return errored(err,chan.err);
-
-          var js = {}
-          if(typeof args.headers == "object") Object.keys(args.headers).forEach(function(header){
-            js[header.toLowerCase()] = args.headers[header].toString();
-          });
-          js.status = args.status || 200;
-          if(args.json) args.body = JSON.stringify(args.json);
-          if(args.body) js["content-length"] = args.body.length.toString();
-          var phttp = self.pencode(js,args.body);
-
-          var js = {};
-          if(args.body && phttp.length <= 1000) js.end = true;
-          chan.send({js:js,body:phttp.slice(0,1000)});
-          if(phttp.length >1000) pipe.write(phttp.slice(1000));
-          return pipe;
-        });
-        if(http.body) pipe.push(http.body);
-        if(err) pipe.emit("end");
-      }
-      chan.callback(err,packet,chan,cbStart);
-    }
-  }
-  
-  var mPaths = {};
-  self.thtp.match = function(uri, cbMatch)
-  {
-    var path = self.uriparse(uri).pathname;
-    mPaths[path] = cbMatch;
-    if(Object.keys(mPaths).length > 1) return;
-    self.thtp.listen(function(req,cbRes){
-      var match;
-      Object.keys(mPaths).forEach(function(path){
-        if(req.path.indexOf(path) != 0) return;
-        if(match && match.length > path) return; // prefer longest match
-        match = path;
-      })
-//      console.log("CHECKING",req.path,match);
-      if(match) return mPaths[match](req,cbRes);
-      cbRes({status:404,body:"not found"});
-    });
-  }
-  
-  // this is super simplistic, it'll need a lot of edge case fixes
-  self.thtp.proxy = function(args)
-  {
-    if(args.address == "0.0.0.0") args.address = "127.0.0.1";
-    self.thtp.listen(function(req,cbRes){
-      var opt = {host:args.address,port:args.port,headers:req.headers,method:req.method,path:req.path};
-      req.pipe(httplib.request(opt, function(res){
-        res.pipe(cbRes({status:res.statusCode,headers:res.headers}));
-      }));
-    });
-  }
-  */
-
-}
-
-// convenience wrapper
-function errored(err, cb)
-{
-  cb(err);
-  var pipe = stream.Readable();
-  pipe._read = function(){}; // TODO
-  pipe.emit("end");
-  return pipe;
-}
-
-function streamer(chan)
-{
-  var pipe = stream.Duplex();
-  pipe._read = function(){}; // TODO
-  pipe.on("finish",function(){
-    chan.send({js:{end:true}});
-  });
-  pipe._write = function(data,enc,cbWrite)
-  {
-    // chunk it
-    while(data.length)
-    {
-      var chunk = data.slice(0,1000);
-      data = data.slice(1000);
-      var packet = {js:{},body:chunk};
-      // last packet gets confirmed/flag
-      if(!data.length)
-      {
-        packet.callback = cbWrite;
-        if(pipe.ended) packet.js.end = true;
-      }
-      chan.send(packet);
-    }
-  }
-  pipe.end = function(data)
-  {
-    pipe.ended = true;
-    if(!data) data = new Buffer(0);
-    pipe.write(data);
-  }
-  return pipe;
 }
--- test/ext/thtp.test.js
+++ test/ext/thtp.test.js
@@ -1,6 +1,8 @@
 var expect = require('chai').expect;
+var concat = require('concat-stream');
 var telehash = require('../../lib/mesh.js');
 var lob = require('lob-enc');
+var stream = require('../../ext/stream.js');
 var thtp = require('../../ext/thtp.js');
 
 describe('telehash/thtp', function(){
@@ -13,13 +15,53 @@ describe('telehash/thtp', function(){
     expect(thtp.mesh).to.be.a('function');
   });
 
-  it('should create a new extension', function(){
-    thtp.mesh({}, function(err, ext){
+  it('should create a new extension', function(done){
+    var mesh = {};
+    thtp.mesh(mesh, function(err, ext){
       expect(err).to.not.exist;
       expect(ext).to.be.a('object');
       expect(ext.link).to.be.a('function');
+      expect(mesh.proxy).to.be.a('function');
+      expect(mesh.request).to.be.a('function');
+      var link = {};
+      ext.link(link, function(err){
+        expect(err).to.not.exist;
+        expect(link.proxy).to.be.a('function');
+        expect(link.request).to.be.a('function');
+        done();
+      })
     });
   });
 
+  it('should receive a proxy request', function(done){
+//    telehash.log({debug:console.log});
+    var meshA = telehash.mesh({id:idA,extensions:{stream:stream,thtp:thtp}});
+    expect(meshA).to.exist;
+    var meshB = telehash.mesh({id:idB,extensions:{stream:stream,thtp:thtp}});
+    expect(meshB).to.exist;
+    
+    // pair them
+    meshA.mesh(meshB);
+    var linkAB = meshA.link(meshB.hashname);
+    
+    // dummy proxy
+    var proxy = {emit:function(on, req, res){
+      expect(on).to.be.equal('request');
+      expect(req.method).to.be.equal('POST');
+      expect(req.url).to.be.equal('/');
+      req.pipe(concat(function(body){
+        expect(body).to.exist;
+        expect(body.toString()).to.be.equal('test');
+        done();
+      }));
+    }};
+    meshB.proxy(proxy);
+    
+    // send request
+    linkAB.request({method:'post',path:'/'}, function(err){
+      expect(err).to.not.exist;
+    }).end('test');
+
+  });
 
 });

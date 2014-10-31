bc30fe8f5f9b9d367c1a4ae5ddc1f4a200a481d1
Jeremie Miller <jeremie@jabber.org>
2014-10-30 23:59:07 -0600

    add a link util for testing to read stdin and link to it

---
+++ bin/link.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+// simple testing utility to take any link json on STDIN and try to link to it, then exit on success/error
+
+var telehash = require('../lib/mesh.js');
+telehash.add(require('telehash-udp4'));
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function error(err)
+{
+  console.error(err);
+  process.exit(1);
+}
+
+telehash.generate(function(err, id){
+  if(err) return error(err);
+  telehash.mesh({id:id}, function(err, mesh){
+    if(err) return error(err);
+    rl.on('line', function(line){
+      var link = mesh.link(JSON.parse(line));
+      link.status(function(err){
+        if(err) return error(err);
+        process.exit(0);
+      });
+      setTimeout(function(){error('timeout')},1000);
+    });
+  });
+});
+
+
+

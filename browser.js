a2632d3c9a2309b5b255694b7430b58159b76e60
Jeremie Miller <jeremie@jabber.org>
2015-01-29 12:15:40 -0700

    basic browser manual test working

--- browser.js
+++ browser.js
@@ -2,6 +2,6 @@
 var telehash = window.telehash = require('./index');
 
 // enable logging by default (at this stage)
-telehash.log({debug:function(){console.log( Array.prototype.slice.call(arguments) );}});
+telehash.log({debug:function(){console.log.apply(console, arguments);}});
 
 console.log('telehash loaded');

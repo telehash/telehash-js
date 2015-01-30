18df6ad5a71331ec08effe8e35d0f4b216cd236d
Jeremie Miller <jeremie@jabber.org>
2015-01-30 12:52:54 -0700

    also log changes

--- bin/router.js
+++ bin/router.js
@@ -14,4 +14,11 @@ repl.start(argv, function(mesh){
 
   mesh.rlog('router:',mesh.json());
   mesh.rlog('router:',mesh.uri());
+
+  // for any links, we log status changes
+  mesh.extending({link:function(link){
+    link.status(function(err){
+      mesh.rlog(link.hashname.substr(0,8),err?'down':'up',err||'');
+    });
+  }});
 });

8731ed5bf3f9a1b34e06cb1b2ae0299b9b973e4c
Jeremie Miller <jeremie@jabber.org>
2015-02-20 12:16:16 -0700

    join chat

--- bin/chat.js
+++ bin/chat.js
@@ -4,6 +4,7 @@ var repl = require('../lib/repl.js');
 
 var argv = require('optimist')
   .boolean('v').describe('v', 'verbose')
+  .describe('nick', 'nickname')
   .argv;
 
 argv.eval = function(cmd, context, filename, callback) {
@@ -15,6 +16,14 @@ repl.start(argv, function(mesh){
   mesh.discover(true);
   mesh.accept = mesh.link; // auto-link any
 
-  mesh.rlog('chat:',mesh.uri());
+  // create a chat
+  var args = argv._[0] || {}; // from given uri, or blank
+  mesh.chat(args, function(err, chat){
+    if(err) return mesh.rlog('err',err);
+    chat.join(argv.nick,function(err){
+      if(argv._[0]) mesh.rlog('joined');
+      else mesh.rlog('invite others with',mesh.uri({protocol:'chat',token:chat.id}));
+    });
+  });
 
 });

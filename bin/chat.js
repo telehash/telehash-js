82ce1ffde0db71f5f24a89193a5f1d7ed094bb25
Jeremie Miller <jeremie@jabber.org>
2015-02-27 08:48:22 -0700

    refactor to use readline instead of the repl, much cleanup to do yet

--- bin/chat.js
+++ bin/chat.js
@@ -1,45 +1,76 @@
 #!/usr/bin/env node
 
-var repl = require('../lib/repl.js');
-
 var argv = require('optimist')
   .boolean('v').describe('v', 'verbose')
   .describe('nick', 'nickname')
   .argv;
 
-var chat;
-argv.eval = function(cmd, context, filename, callback) {
-  if(!chat) return callback(null, 'not connected');
-  cmd = cmd.slice(1,cmd.length-1).trim(); // goop from REPL lib
-  
-  // no command so we just print list of connected
-  if(cmd == '')
-  {
-    var list = Object.keys(chat.profiles).map(function(hn){return chat.profiles[hn].json.text});
-    if(list.length == 0) list.push('nobody');
-    callback(null, list.join(', '));
-    return;
-  }
+var fs = require('fs');
+var path = require('path-extra');
+var readline = require('readline');
+var telehash = require('..');
 
-  // send any raw text out to the chat
-  chat.outbox.write(cmd);
-  callback(null, 'sent');
+// localize our id file
+argv.id = (argv.id) ? path.resolve(argv.id) : path.join(path.homedir(),'.chat.json');
+
+if(argv.v)
+{
+  telehash.log({debug:console.log});
+  console.log('starting with args',argv);
 }
 
-repl.start(argv, function(mesh){
+telehash.load(argv, function(err, mesh){
+  if(err) console.error('something went wrong :(',err,argv);
+  if(!mesh) process.exit(1);
+  
+  if(!argv.nick) argv.nick = mesh.hashname.substr(0,8);
+  
   mesh.router(true);
   mesh.discover(true);
   mesh.accept = mesh.link; // auto-link any
 
   // create a chat
   var args = argv._[0] || {}; // from given uri, or blank for new one
-  mesh.chat(args, argv.nick, function(err, ref){
-    chat = ref;
-    if(err) mesh.rlog('err',err);
+  mesh.chat(args, argv.nick, function(err, chat){
+    if(err) console.error(err);
     if(!chat) process.exit(1);
 
-    if(argv._[0]) mesh.rlog('connected');
-    else mesh.rlog('invite others with',mesh.uri({protocol:'chat',token:chat.id}));
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.setPrompt(argv.nick+'> ');
+    rl.on('close', function () {
+      // TODO any nice shutdown?
+      console.log(); // nice newline
+      process.exit(0);
+    });
+
+    function rlog()
+    {
+      console.log.apply(console, arguments);
+      rl.prompt();
+    }
+
+    if(argv._[0]) rlog('connected');
+    else rlog('invite others with',mesh.uri({protocol:'chat',token:chat.id}));
+    rl.prompt();
+
+    rl.on('line', function(cmd){
+      // no command so we just print list of connected
+      if(cmd == '')
+      {
+        var list = Object.keys(chat.profiles).map(function(hn){return chat.profiles[hn].json.text});
+        if(list.length == 0) list.push('nobody');
+        rlog(list.join(', '));
+        return;
+      }
+      rl.pause();
+      // send any raw text out to the chat
+      chat.outbox.write(cmd);
+      setTimeout(function(){rlog('sent');},1000);    
+    });
+
     
     // process incoming messages
     chat.nicks = {};
@@ -48,15 +79,15 @@ repl.start(argv, function(mesh){
       var nick = (chat.profiles[msg.from]) ? chat.profiles[msg.from].json.text : 'unknown';
       if(msg.json.type == 'join')
       {
-        mesh.rlog(nick,'just joined');
+        rlog(nick,'just joined');
       }
       if(msg.json.type == 'profile')
       {
-        mesh.rlog('accepting',msg.json.text,msg.from);
+        rlog('accepting',msg.json.text,msg.from);
         chat.join(mesh.link(msg.from));
       }
-      if(msg.from != mesh.hashname && msg.json.type == 'chat') mesh.rlog(nick+': '+msg.json.text);
+      if(msg.from != mesh.hashname && msg.json.type == 'chat') rlog(nick+': '+msg.json.text);
     });
   });
-
+  
 });

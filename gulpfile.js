9097ccf2a4efc9ba7a5d4c2342dc77262019f7e0
Ryan Bennett <nomad.ry@gmail.com>
2015-06-22 14:40:22 -0600

    add gulpfile! test chrome thoroughly with chext and watch to trigger mocha tests

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var watch = require('gulp-watch')
+var chext = require("chext")
+
+var unitTests = [ 'test/index.test.js'
+                , 'test/ext/box.test.js'
+                , 'test/ext/chat.test.js'
+                , 'test/ext/path.test.js'
+                , 'test/ext/peer.test.js'
+                , 'test/ext/stream.test.js'
+                , 'test/ext/thtp.test.js'
+                , 'test/lib/mesh.test.js'
+                , 'test/lib/pipe.test.js'
+                , 'test/lib/uri.test.js'];
+
+var srcFiles = ['index.js', 'ext/*.js', 'lib/*.js']
+
+
+gulp.task('mocha', function() {
+  return  gulp.src( unitTests, { read: false })
+              .pipe(mocha({ reporter: 'list' }))
+              .on('error', function(){
+              })
+              .on('end', function(){
+
+              });
+});
+
+gulp.task('dev', ['mocha'], function() {
+  var ch = new chext()
+
+  ch.watchify(unitTests)
+
+  ch.on("testing_complete",function(results){
+    console.log("tests complete", results)
+  })
+
+  gulp.watch(unitTests.concat(srcFiles), ["mocha"])
+})

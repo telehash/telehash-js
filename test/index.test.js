d4f4b17bc0ae03768fa44376ac2f541499541da2
Ryan Bennett <nomad.ry@gmail.com>
2015-07-09 10:14:58 -0600

    comment out index test logging

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 var expect = require('chai').expect;
 var telehash = require('..');
-telehash.log({debug:console.log});
+//telehash.log({debug:console.log});
 describe('telehash-js', function(){
 
   var idA = {"keys":{"1a":"akndnx5kbansip6xphxymwckpqkjj26fcm"},"secrets":{"1a":"ksxslm5mmtymnbph7nvxergb7oy3r35u"},"hashname":"5uegloufcyvnf34jausszmsabbfbcrg6fyxpcqzhddqxeefuapvq"};
@@ -13,7 +13,7 @@ describe('telehash-js', function(){
   it('should have extensions', function(){
     expect(Object.keys(telehash.extensions).length).to.be.above(3);
   });
-  
+
   it('should create a real mesh', function(done){
     telehash.mesh({id:idA},function(err, mesh){
       expect(err).to.not.exist;

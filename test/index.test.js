573bc5dfb6f65235131a30a65b4f1919877517df
Ryan Bennett <nomad.ry@gmail.com>
2015-07-10 14:37:40 -0600

    remove some console logs

--- test/index.test.js
+++ test/index.test.js
@@ -39,15 +39,10 @@ describe('telehash-js', function(){
         expect(err).to.not.exist;
         var linkBA = meshB.link({keys:idA.keys,paths:meshA.paths()});
         expect(linkAB).to.exist;
-        console.log("status triggered")
         linkBA.on('status',function(err){
-          console.log("status event")
-        })
-        linkBA.status(function(err){
-          console.log("status triggered")
           expect(err).to.not.exist;
           done();
-        });
+        })
       });
     });
   });
